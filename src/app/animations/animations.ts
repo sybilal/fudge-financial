13e298e53bd8b9e55bf71b23f77899c65a9dbb80
sybilal <sbilalahmed95@gmail.com>
2021-06-26 02:13:57 +0400

    dashboard in progress

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -3,8 +3,7 @@ import { animate, state, style, transition, trigger } from "@angular/animations"
 export let movelogo =
     // the fade-in/fade-out animation.
     trigger('movelogo', [
-        state('raised', style({ transform: 'translate(-50%,-957%)' })),
-        state('cent', style({ transform: 'translate(-50%, -600%)' })),
+        state('raised', style({ transform: 'translate(-50%,-300%)' })),
         transition('cent => raised', animate('1000ms')),
     ]);
 
@@ -12,15 +11,16 @@ export let movelogo =
 export let raiseContent =
     // the fade-in/fade-out animation.
     trigger('raiseContent', [
-        state('shown', style({ transform: 'translate(-50%,-230%)', opacity: 1 })),
-        state('hidden', style({ transform: 'translate(-50%,0%);', opacity: 0 })),
+        // state('shown', style({ transform: 'translate(-50%,-230%)', opacity: 1 })),
+        state('shown', style({ bottom: 'calc(50% - 50px)', opacity: 1 })),
+        // state('hidden', style({ transform: 'translate(-50%,0%);', opacity: 0 })),
         transition('hidden => shown', animate('1000ms')),
     ]);
 export let raiseButton =
     // the fade-in/fade-out animation.
     trigger('raiseButton', [
-        state('shown', style({ transform: 'translate(-50%,-500%)', opacity: 1 })),
-        state('hidden', style({ transform: 'translate(-50%,0%);', opacity: 0 })),
+        state('shown', style({ bottom: 'calc(50% - 150px)', opacity: 1 })),
+        // state('hidden', style({ transform: 'translate(-50%,0%);', opacity: 0 })),
         transition('hidden => shown', animate('1000ms')),
     ]);
 
